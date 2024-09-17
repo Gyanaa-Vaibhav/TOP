a11c4af0ef92a1d9e5d849794b6c49fc85bfc88e
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-09-17 15:02:39 +0530

    Pock Paper Scissor Game in the Console

---
+++ Javascript/RPS_game/script.js
@@ -0,0 +1,48 @@
+// alert("HI")
+
+function getComputerChoice(){
+    let roll = Math.floor(Math.random() * 3);
+    if (roll == 0) {
+        return "ROCK"
+    } else if (roll == 1) {
+        return "PAPER"
+    } else {
+        return "SCISSORS"
+    }
+}
+
+
+function getHumanChoice() {
+    let sign = prompt("What's your Choice?")
+    return sign;
+}
+
+
+let humanScore;
+let computerScore;
+
+function playround(humanchoice, computerchoice) {
+    console.log(computerchoice)
+    console.log(humanchoice);
+
+    if (humanchoice == "ROCK" && computerchoice == "SCISSORS") {
+        console.log("You Win");
+        humanScore++;
+    } else if (humanchoice == "PAPER" && computerchoice == "ROCK") {
+        console.log("You Win");
+        humanScore++;
+    } else if (humanchoice == "SCISSORS" && computerchoice == "PAPER") {
+        console.log("You Win");
+        humanScore++;
+    } else if (humanchoice == computerScore) {
+        console.log("Draw");
+    } else {
+        console.log("You Lose");
+        
+    }
+    console.log("Working");
+    
+}
+
+
+playround(getHumanChoice(),getComputerChoice())
