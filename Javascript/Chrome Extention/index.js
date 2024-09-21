e8c814f8d2c8b5a51151a0c40bb8a5a85d70907f
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-09-21 22:17:11 +0530

    Eveything works expect the backend unable to figure out why the clock stops when it is in the background will check once back to the backend section

--- Javascript/Chrome Extention/index.js
+++ Javascript/Chrome Extention/index.js
@@ -5,56 +5,101 @@ const long = document.getElementById("long")
 const main = document.querySelector(".main")
 const theme = document.getElementsByTagName("link")[0];
 const timer = document.querySelector(".timer")
+const sCount = document.querySelector(".counter")
+let count = 0;
+let timeCounter = 1500000;
+var x;
 
-const startTimer = function () {
-    setTimeout(() => {
-        alert("Working YAY")
-    }, 1000);
+if (localStorage.getItem("timer")) {
+  timer.textContent = localStorage.getItem("timer")
 }
 
-startBtn.addEventListener("click", startTimer)
-
 pomo.addEventListener("click", function () {
-    theme.setAttribute("href", "./css/index.css")
-    // console.log(timer.value)
-    timer.textContent = "25:00";
+  theme.setAttribute("href", "./css/index.css")
+  // console.log(timer.value)
+  timeCounter = 1500000
+    if (localStorage.getItem("time")) {
+    timer.textContent = localStorage.getItem("time");
+  } else {
+    timer.textContent = "25:00"
+  }
 })
 
 short.addEventListener("click", function () {
-    theme.setAttribute("href", "./css/short_break.css")
-    timer.textContent = "5:00";
+  theme.setAttribute("href", "./css/short_break.css")
+  timeCounter = 300000
+    if (localStorage.getItem("time")) {
+    timer.textContent = localStorage.getItem("time");
+  } else {
+    timer.textContent = "5:00"
+  }
 })
 
 long.addEventListener("click", function () {
-    theme.setAttribute("href", "./css/long_break.css")
-    timer.textContent = "15:00";
+  theme.setAttribute("href", "./css/long_break.css")
+  timeCounter = 900000
+  if (localStorage.getItem("time")) {
+    timer.textContent = localStorage.getItem("time");
+  } else {
+    timer.textContent = "15:00"
+  }
 })
 
+if (localStorage.getItem("time")) {
+  timer.textContent = localStorage.getItem("time");
+} else {
+  timer.textContent = "25:00"
+}
 
-var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+const startTimer = function () {
+  t = 0;
+  var x = setInterval(function () {
+    var distance = timeCounter - t;
+    console.log(distance)
+    
+    // Time calculations for minutes and seconds
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    console.log(minutes)
+    console.log(seconds)
+    
+    // Output the result in an element with id="demo"
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+    if (minutes < 10 && seconds < 10) {
+      let t = document.querySelector(".timer").textContent = "0" + minutes + ":0" + seconds;
+      localStorage.setItem("time", t)
+    } else if (seconds < 10) {
+      let t = document.querySelector(".timer").textContent = minutes + ":" + seconds + "0";
+      localStorage.setItem("time", t)
+    } else if (minutes < 10) {
+      let t = document.querySelector(".timer").textContent = `0${minutes}:${seconds}`;
+      localStorage.setItem("time", t)
+    } else {
+      let t = document.querySelector(".timer").textContent = `${minutes}:${seconds}`;
+      localStorage.setItem("time", t)
+    }
+    
+    if (distance < 0) {
+      clearInterval(x);
+      document.querySelector(".timer").textContent = "25:00";
+      count ++;
+      sCount.textContent += count;
+      localStorage.clear()
+    }
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+    startBtn.addEventListener("click", function () {
+      clearInterval(x);
+      localStorage.clear();
+      timer.textContent = "25:00"
+    })
+
+    t += 1000;
+
+  }, 1000);
+}
+
+startBtn.addEventListener("dblclick", function () {
+  startTimer();
+  startBtn.textContent = "Stop";
+})
+
