81fd88ee5dc1ed2e7f0cfb881a5ff80505d88cc5
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-09-25 23:53:53 +0530

    Updates to the RPS game improved the Styling and made it more playable

--- Javascript/RPS_game/script.js
+++ Javascript/RPS_game/script.js
@@ -1,4 +1,12 @@
-// alert("HI")
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+const cans = document.getElementById("computer-ans");
+const ans = document.getElementById("ans");
+const attempts = document.getElementById("attempts");
+let humanScore;
+let computerScore;
+let attempt = 0;
 
 function getComputerChoice(){
     let roll = Math.floor(Math.random() * 3);
@@ -11,37 +19,45 @@ function getComputerChoice(){
     }
 }
 
-
-function getHumanChoice() {
-    let sign = prompt("What's your Choice?")
-    return sign;
-}
-
-
-let humanScore;
-let computerScore;
-
 function playround(humanchoice, computerchoice) {
     console.log(computerchoice)
     console.log(humanchoice);
+    attempt ++;
+    attempts.textContent = `Attempts Made ${attempt}`
+
+    cans.textContent = `Computer Chose : ${computerchoice}`;
 
     if (humanchoice == "ROCK" && computerchoice == "SCISSORS") {
         console.log("You Win");
+        ans.textContent = "You Won Congratulations!";
         humanScore++;
     } else if (humanchoice == "PAPER" && computerchoice == "ROCK") {
         console.log("You Win");
+        ans.textContent = "You Won Congratulations!";
         humanScore++;
     } else if (humanchoice == "SCISSORS" && computerchoice == "PAPER") {
         console.log("You Win");
+        ans.textContent = "You Won Congratulations!";
         humanScore++;
-    } else if (humanchoice == computerScore) {
+    } else if (humanchoice === computerchoice) {
         console.log("Draw");
+        ans.textContent = "Its a Draw"
     } else {
         console.log("You Lose");
-        
+        ans.textContent = "You Lose"
     }
     
 }
 
+rock.addEventListener("click", function () {
+    playround("ROCK", getComputerChoice())
+    
+})
 
-playround(getHumanChoice(),getComputerChoice())
+paper.addEventListener("click", function () {
+    playround("PAPER",getComputerChoice())
+})
+
+scissor.addEventListener("click", function () {
+    playround("SCISSORS",getComputerChoice())
+})
